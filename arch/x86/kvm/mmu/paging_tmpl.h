a8a7f2a7a99067e72eb91a5ad68acc52a5a89107
Sean Christopherson <seanjc@google.com>
2021-11-20 04:50:22 +0000

    KVM: x86/mmu: Retry page fault if root is invalidated by memslot update
    commit a955cad84cdaffa282b3cf8f5ce69e9e5655e585 upstream.
    
    Bail from the page fault handler if the root shadow page was obsoleted by
    a memslot update.  Do the check _after_ acuiring mmu_lock, as the TDP MMU
    doesn't rely on the memslot/MMU generation, and instead relies on the
    root being explicit marked invalid by kvm_mmu_zap_all_fast(), which takes
    mmu_lock for write.
    
    For the TDP MMU, inserting a SPTE into an obsolete root can leak a SP if
    kvm_tdp_mmu_zap_invalidated_roots() has already zapped the SP, i.e. has
    moved past the gfn associated with the SP.
    
    For other MMUs, the resulting behavior is far more convoluted, though
    unlikely to be truly problematic.  Installing SPs/SPTEs into the obsolete
    root isn't directly problematic, as the obsolete root will be unloaded
    and dropped before the vCPU re-enters the guest.  But because the legacy
    MMU tracks shadow pages by their role, any SP created by the fault can
    can be reused in the new post-reload root.  Again, that _shouldn't_ be
    problematic as any leaf child SPTEs will be created for the current/valid
    memslot generation, and kvm_mmu_get_page() will not reuse child SPs from
    the old generation as they will be flagged as obsolete.  But, given that
    continuing with the fault is pointess (the root will be unloaded), apply
    the check to all MMUs.
    
    Fixes: b7cccd397f31 ("KVM: x86/mmu: Fast invalidation for TDP MMU")
    Cc: stable@vger.kernel.org
    Cc: Ben Gardon <bgardon@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20211120045046.3940942-5-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    [ doebel: 5.15.y predates the kvm_page_fault struct and the is_page_fault_stale()
      helper (direct_page_fault()/FNAME(page_fault)() still take gpa/error_code
      arguments rather than a struct kvm_page_fault), so the cherry-pick does not
      apply cleanly.  5.15.y already uses write_lock/read_lock + to_shadow_page() +
      mmu_notifier_retry_hva(), so only the helper is missing.  Open-code the
      is_obsolete_sp() check just before the existing mmu_notifier_retry_hva()
      early-out in direct_page_fault() and FNAME(page_fault)() (same ordering as the
      upstream is_page_fault_stale() helper).  The is_obsolete_sp() hunk applies
      verbatim so the check skips the mmu_valid_gen comparison for sp->tdp_mmu_page
      pages. ]
    Signed-off-by: Bjoern Doebel <doebel@amazon.de>

--- arch/x86/kvm/mmu/paging_tmpl.h
+++ arch/x86/kvm/mmu/paging_tmpl.h
@@ -923,6 +923,8 @@ static int FNAME(page_fault)(struct kvm_vcpu *vcpu, gpa_t addr, u32 error_code,
 
 	r = RET_PF_RETRY;
 	write_lock(&vcpu->kvm->mmu_lock);
+	if (is_obsolete_sp(vcpu->kvm, to_shadow_page(vcpu->arch.mmu->root_hpa)))
+		goto out_unlock;
 	if (!is_noslot_pfn(pfn) && mmu_notifier_retry_hva(vcpu->kvm, mmu_seq, hva))
 		goto out_unlock;
 
