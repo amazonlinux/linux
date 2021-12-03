26dad8a38397abf9176aabadf38e8a83add1c7af
Maxim Galaganov <max@internet.ru>
2021-12-03 14:35:41 -0800

    mptcp: support TCP_CORK and TCP_NODELAY
    commit 4f6e14bd19d6de7831f31cfb3210f2ea93eeb038 upstream.
    
    First, add cork and nodelay fields to the mptcp_sock structure
    so they can be used in sync_socket_options(), and fill them on setsockopt
    while holding the msk socket lock.
    
    Then, on setsockopt set proper tcp_sk(ssk)->nonagle values for subflows
    by calling __tcp_sock_set_cork() or __tcp_sock_set_nodelay() on the ssk
    while holding the ssk socket lock.
    
    tcp_push_pending_frames() will be invoked on the ssk if a cork was cleared
    or nodelay was set. Also set MPTCP_PUSH_PENDING bit by calling
    mptcp_check_and_set_pending(). This will lead to __mptcp_push_pending()
    being called inside mptcp_release_cb() with new tcp_sk(ssk)->nonagle.
    
    Also add getsockopt support for TCP_CORK and TCP_NODELAY.
    
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Maxim Galaganov <max@internet.ru>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 4f6e14bd19d6de7831f31cfb3210f2ea93eeb038)

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -252,7 +252,9 @@ struct mptcp_sock {
 	bool		use_64bit_ack; /* Set when we received a 64-bit DSN */
 	bool		csum_enabled;
 	bool		allow_infinite_fallback;
-	u8		recvmsg_inq:1;
+	u8		recvmsg_inq:1,
+			cork:1,
+			nodelay:1;
 	spinlock_t	join_list_lock;
 	int		keepalive_cnt;
 	int		keepalive_idle;
