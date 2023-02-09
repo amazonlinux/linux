615116e7bc4a1fa95915922efc56c7960ae9b9ed
David de Bruyn <daviddb@amazon.com>
2023-02-09 09:19:09 +0000

    Enable ptIOMMU for all supported platforms
    Currently ptIOMMU functionality is only enabled for ARM64. This patch
    removes the architecture specific code to enable the ptIOMMU and
    replaces it with an achitecture-independent strategy.
    
    Signed-off-by: David de Bruyn <daviddb@amazon.com>
    Signed-off-by: Hazem Mohamed Abuelfotoh <abuehaze@amazon.com>
    Signed-off-by: Nathan Gao <zcgao@amazon.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -14,6 +14,7 @@
 #include <linux/acpi_rimt.h>
 #include <linux/acpi_viot.h>
 #include <linux/iommu.h>
+#include <linux/dma-page-touching.h>
 #include <linux/signal.h>
 #include <linux/kthread.h>
 #include <linux/dmi.h>
@@ -1684,6 +1685,11 @@ int acpi_dma_configure_id(struct device *dev, enum dev_dma_attr attr,
 		dev_dbg(dev, "Adding to IOMMU failed: %d\n", ret);
 
 	arch_setup_dma_ops(dev, attr == DEV_DMA_COHERENT);
+
+#ifdef CONFIG_DMA_PAGE_TOUCHING
+	if (!dev->dma_ops)
+		setup_dma_page_touching_ops(dev);
+#endif
 
 	return 0;
 }
