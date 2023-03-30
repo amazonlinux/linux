6ff45d26359bdb9a9020a5eb210a2812df02712b
Marc Zyngier <maz@kernel.org>
2023-03-30 18:47:45 +0100

    KVM: arm64: timers: Allow physical offset without CNTPOFF_EL2
    CNTPOFF_EL2 is awesome, but it is mostly vapourware, and no publicly
    available implementation has it. So for the common mortals, let's
    implement the emulated version of this thing.
    
    It means trapping accesses to the physical counter and timer, and
    emulate some of it as necessary.
    
    As for CNTPOFF_EL2, nobody sets the offset yet.
    
    [Backport from c605ee245097 to 5.10. Resolved conlicts by adding
    incoming SYS_CNTPCT* registers and added access_arch_timer to
    cp15_64_regs[]. Removed is_protected_kvm_enabled case in
    __timer_enable_traps since the feature was added later[1].]
    
    [1] https://lore.kernel.org/r/20201202184122.26046-3-dbrazdil@google.com
    
    Reviewed-by: Colton Lewis <coltonlewis@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20230330174800.2677007-6-maz@kernel.org
    Signed-off-by: Nathan Gao <zcgao@amazon.com>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -453,6 +453,10 @@
 
 #define SYS_CNTFRQ_EL0			sys_reg(3, 3, 14, 0, 0)
 
+#define SYS_CNTPCT_EL0			sys_reg(3, 3, 14, 0, 1)
+#define SYS_CNTPCTSS_EL0		sys_reg(3, 3, 14, 0, 5)
+#define SYS_CNTVCTSS_EL0		sys_reg(3, 3, 14, 0, 6)
+
 #define SYS_CNTP_TVAL_EL0		sys_reg(3, 3, 14, 2, 0)
 #define SYS_CNTP_CTL_EL0		sys_reg(3, 3, 14, 2, 1)
 #define SYS_CNTP_CVAL_EL0		sys_reg(3, 3, 14, 2, 2)
@@ -462,6 +466,7 @@
 
 #define SYS_AARCH32_CNTP_TVAL		sys_reg(0, 0, 14, 2, 0)
 #define SYS_AARCH32_CNTP_CTL		sys_reg(0, 0, 14, 2, 1)
+#define SYS_AARCH32_CNTPCT		sys_reg(0, 0, 0, 14, 0)
 #define SYS_AARCH32_CNTP_CVAL		sys_reg(0, 2, 0, 14, 0)
 
 #define __PMEV_op2(n)			((n) & 0x7)
