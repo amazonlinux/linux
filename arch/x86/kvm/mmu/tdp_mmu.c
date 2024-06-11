e3e3e88b28c3366d14f354374492eae7228ae7e2
David Matlack <dmatlack@google.com>
2024-06-11 15:05:12 -0700

    KVM: x86/mmu: Avoid reacquiring RCU if TDP MMU fails to allocate an SP
    Avoid needlessly reacquiring the RCU read lock if the TDP MMU fails to
    allocate a shadow page during eager page splitting. Opportunistically
    drop the local variable ret as well now that it's no longer necessary.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Link: https://lore.kernel.org/r/20240611220512.2426439-5-dmatlack@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1403,7 +1403,6 @@ static int tdp_mmu_split_huge_pages_root(struct kvm *kvm,
 {
 	struct kvm_mmu_page *sp = NULL;
 	struct tdp_iter iter;
-	int ret = 0;
 
 	rcu_read_lock();
 
@@ -1441,15 +1440,14 @@ retry:
 			else
 				write_lock(&kvm->mmu_lock);
 
-			rcu_read_lock();
-
 			if (!sp) {
-				ret = -ENOMEM;
 				trace_kvm_mmu_split_huge_page(iter.gfn,
 							      iter.old_spte,
-							      iter.level, ret);
-				break;
+							      iter.level, -ENOMEM);
+				return -ENOMEM;
 			}
+
+			rcu_read_lock();
 
 			iter.yielded = true;
 			continue;
@@ -1473,7 +1471,7 @@ retry:
 	if (sp)
 		tdp_mmu_free_sp(sp);
 
-	return ret;
+	return 0;
 }
 
 
