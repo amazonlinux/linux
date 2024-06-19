e7bb4161f788b2ca5a3302384a7e503114d3de8d
James Gowans <jgowans@amazon.com>
2024-06-19 11:55:55 +0200

    memblock: Move late alloc warning down to phys alloc
    If a driver/subsystem tries to do an allocation after the memblock
    allocations have been freed and the memory handed to the buddy
    allocator, it will not actually be legal to use that allocation: the
    buddy allocator owns the memory. Currently this mis-use is handled by
    the memblock function which does allocations and returns virtual
    addresses by printing a warning and doing a kmalloc instead. However
    the physical allocation function does not to do this check - callers of
    the physical alloc function are unprotected against mis-use.
    
    Improve the error catching here by moving the check into the physical
    allocation function which is used by the virtual addr allocation
    function.
    
    Signed-off-by: James Gowans <jgowans@amazon.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Alex Graf <graf@amazon.de>
    Link: https://lore.kernel.org/r/20240619095555.85980-1-jgowans@amazon.com
    Signed-off-by: Mike Rapoport (IBM) <rppt@kernel.org>

--- mm/memblock.c
+++ mm/memblock.c
@@ -1439,6 +1439,17 @@ phys_addr_t __init memblock_alloc_range_nid(phys_addr_t size,
 	enum memblock_flags flags = choose_memblock_flags();
 	phys_addr_t found;
 
+	/*
+	 * Detect any accidental use of these APIs after slab is ready, as at
+	 * this moment memblock may be deinitialized already and its
+	 * internal data may be destroyed (after execution of memblock_free_all)
+	 */
+	if (WARN_ON_ONCE(slab_is_available())) {
+		void *vaddr = kzalloc_node(size, GFP_NOWAIT, nid);
+
+		return vaddr ? virt_to_phys(vaddr) : 0;
+	}
+
 	if (!align) {
 		/* Can't use WARNs this early in boot on powerpc */
 		dump_stack();
@@ -1564,13 +1575,6 @@ static void * __init memblock_alloc_internal(
 {
 	phys_addr_t alloc;
 
-	/*
-	 * Detect any accidental use of these APIs after slab is ready, as at
-	 * this moment memblock may be deinitialized already and its
-	 * internal data may be destroyed (after execution of memblock_free_all)
-	 */
-	if (WARN_ON_ONCE(slab_is_available()))
-		return kzalloc_node(size, GFP_NOWAIT, nid);
 
 	if (max_addr > memblock.current_limit)
 		max_addr = memblock.current_limit;
