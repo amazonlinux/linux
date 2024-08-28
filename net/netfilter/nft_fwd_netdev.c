9fab87eb225b8ddd5c0be06dbf9fb0b1292d17e5
Florian Westphal <fw@strlen.de>
2024-08-28 11:34:02 +0200

    netfilter: nf_tables: drop unused 3rd argument from validate callback ops
    [ Upstream commit eaf9b2c875ece22768b78aa38da8b232e5de021b ]
    
    Since commit a654de8fdc18 ("netfilter: nf_tables: fix chain dependency validation")
    the validate() callback no longer needs the return pointer argument.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Stable-dep-of: 9dbba7e694ec ("netfilter: nft_compat: ebtables emulation must reject non-bridge targets")
    [ nft_meta_bridge and nft_queue lack the set-validate and queue-validate paths,
      nft_reject_netdev.c does not exist, and the (*reduce) callback is absent from
      struct nft_expr_ops. In all cases the simple mechanical removal of the 3rd
      argument was made. ]
    Signed-off-by: Mark Bundschuh <mkbund@amazon.com>

--- net/netfilter/nft_fwd_netdev.c
+++ net/netfilter/nft_fwd_netdev.c
@@ -210,8 +210,7 @@ nla_put_failure:
 }
 
 static int nft_fwd_validate(const struct nft_ctx *ctx,
-			    const struct nft_expr *expr,
-			    const struct nft_data **data)
+			    const struct nft_expr *expr)
 {
 	return nft_chain_validate_hooks(ctx->chain, (1 << NF_NETDEV_INGRESS));
 }
