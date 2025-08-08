e7c1aa2cccdf06b6b795678bbd22dda591593fc4
Jay Wang <wanjay@amazon.com>
2025-08-08 21:19:32 +0000

    fips140: conduct self-tests on registered cryptographic algorithms
    Execute self-tests for all registered FIPS 140 cryptographic algorithms
    during module initialization.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- crypto/fips140/fips140-selftests.c
+++ crypto/fips140/fips140-selftests.c
@@ -20,127 +20,105 @@
 
 #include "fips140-module.h"
 
+/* Forward declarations to avoid including the full testsuites header */
+struct fips140_alg_test_desc {
+	const char *alg;
+	const char *generic_driver;
+	int (*test)(const struct fips140_alg_test_desc *desc, const char *driver,
+		    u32 type, u32 mask);
+	int fips_allowed;
+	/* We don't need the union for our purposes */
+};
+
+/* External declarations from fips140-testsuites.c */
+extern const struct fips140_alg_test_desc fips140_alg_test_descs[];
+extern const int fips140_alg_test_descs_count;
+
+/* Use the binary search function from testsuites.c */
+extern int fips140_alg_find_test(const char *alg);
+
+/* External declarations from fips140-alg-registration.c */
+extern void fips140_get_registered_algorithms(struct list_head **algs_list, struct mutex **list_mutex);
+
+/* Structure for registered algorithms (from fips140-alg-registration.c) */
+struct fips140_registered_alg {
+	struct list_head list;
+	char *cra_name;
+	char *cra_driver_name;
+	u32 cra_flags;
+	u32 cra_type;
+	int cra_priority;
+	bool tested;
+	bool test_passed;
+};
+
 /* 
- * This file will contain all the self-tests required for FIPS 140 compliance.
- * For now, we'll implement a basic structure that can be expanded later.
+ * This file runs self-tests for all registered FIPS 140 algorithms
+ * using the test descriptors from fips140-testsuites.c
  */
 
-struct fips140_selftest_alg {
-	const char *driver_name;
-	const char *test_name;
-	int (*test_func)(const struct fips140_selftest_alg *alg);
-};
 
-/* AES Known Answer Tests */
-static int fips140_aes_ecb_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement AES-ECB KAT */
-	return 0;
-}
-
-static int fips140_aes_cbc_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement AES-CBC KAT */
-	return 0;
-}
-
-static int fips140_aes_ctr_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement AES-CTR KAT */
-	return 0;
-}
-
-static int fips140_aes_xts_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement AES-XTS KAT */
-	return 0;
-}
-
-/* SHA Known Answer Tests */
-static int fips140_sha1_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement SHA1 KAT */
-	return 0;
-}
-
-static int fips140_sha256_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement SHA256 KAT */
-	return 0;
-}
-
-static int fips140_sha512_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement SHA512 KAT */
-	return 0;
-}
-
-/* HMAC Known Answer Tests */
-static int fips140_hmac_sha256_test(const struct fips140_selftest_alg *alg)
-{
-	/* Implement HMAC-SHA256 KAT */
-	return 0;
-}
-
-/* List of all self-tests to run */
-static const struct fips140_selftest_alg fips140_selftests[] = {
-	{
-		.driver_name = "ecb(aes)",
-		.test_name = "AES-ECB",
-		.test_func = fips140_aes_ecb_test,
-	}, {
-		.driver_name = "cbc(aes)",
-		.test_name = "AES-CBC",
-		.test_func = fips140_aes_cbc_test,
-	}, {
-		.driver_name = "ctr(aes)",
-		.test_name = "AES-CTR",
-		.test_func = fips140_aes_ctr_test,
-	}, {
-		.driver_name = "xts(aes)",
-		.test_name = "AES-XTS",
-		.test_func = fips140_aes_xts_test,
-	}, {
-		.driver_name = "sha1",
-		.test_name = "SHA-1",
-		.test_func = fips140_sha1_test,
-	}, {
-		.driver_name = "sha256",
-		.test_name = "SHA-256",
-		.test_func = fips140_sha256_test,
-	}, {
-		.driver_name = "sha512",
-		.test_name = "SHA-512",
-		.test_func = fips140_sha512_test,
-	}, {
-		.driver_name = "hmac(sha256)",
-		.test_name = "HMAC-SHA-256",
-		.test_func = fips140_hmac_sha256_test,
-	},
-};
 
 bool __init fips140_run_selftests(void)
 {
-	int i;
+	struct list_head *algs_list;
+	struct mutex *list_mutex;
+	struct fips140_registered_alg *reg_alg;
 	bool all_passed = true;
+	int tests_run = 0;
 
-	pr_info("running self-tests\n");
+	pr_info("running self-tests for registered algorithms\n");
 
-	for (i = 0; i < ARRAY_SIZE(fips140_selftests); i++) {
-		const struct fips140_selftest_alg *alg = &fips140_selftests[i];
+	// Get access to the registered algorithms list
+	fips140_get_registered_algorithms(&algs_list, &list_mutex);
+
+	mutex_lock(list_mutex);
+	list_for_each_entry(reg_alg, algs_list, list) {
+		const struct fips140_alg_test_desc *test_desc = NULL;
 		int err;
 
-		pr_info("self-test: %s\n", alg->test_name);
-		err = alg->test_func(alg);
-		if (err) {
-			pr_err("self-test: %s failed (err=%d)\n",
-			       alg->test_name, err);
-			all_passed = false;
+		// Find matching test descriptor (following testmgr.c logic)
+		int alg_idx = fips140_alg_find_test(reg_alg->cra_name);
+		int driver_idx = fips140_alg_find_test(reg_alg->cra_driver_name);
+		
+		// Prefer driver-specific test over generic algorithm test
+		if (driver_idx >= 0)
+			test_desc = &fips140_alg_test_descs[driver_idx];
+		else if (alg_idx >= 0)
+			test_desc = &fips140_alg_test_descs[alg_idx];
+
+		if (!test_desc || !test_desc->test) {
+			pr_info("self-test: no test found for %s (%s), skipping\n",
+				reg_alg->cra_name, reg_alg->cra_driver_name);
+			continue;
 		}
+
+		pr_info("self-test: running test for %s (%s)\n",
+			reg_alg->cra_name, reg_alg->cra_driver_name);
+
+		err = test_desc->test(test_desc, reg_alg->cra_driver_name,
+				      reg_alg->cra_type, 0);
+		
+		reg_alg->tested = true;
+		if (err) {
+			pr_err("self-test: %s (%s) failed (err=%d)\n",
+			       reg_alg->cra_name, reg_alg->cra_driver_name, err);
+			reg_alg->test_passed = false;
+			all_passed = false;
+		} else {
+			reg_alg->test_passed = true;
+		}
+		tests_run++;
+	}
+	mutex_unlock(list_mutex);
+
+	if (tests_run == 0) {
+		pr_warn("no self-tests were run\n");
+		return false;
 	}
 
 	if (all_passed)
-		pr_info("all self-tests passed\n");
+		pr_info("all %d self-tests passed\n", tests_run);
 	else
 		pr_err("one or more self-tests failed\n");
 
