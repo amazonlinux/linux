ddd077d8e92807b9c4bcc3d78b3f376a860d10df
Eric Dumazet <edumazet@google.com>
2025-08-28 19:58:16 +0000

    net: dst: introduce dst->dev_rcu
    [ Upstream commit caedcc5b6df1b2e2b5f39079e3369c1d4d5c5f50 ]
    
    Followup of commit 88fe14253e18 ("net: dst: add four helpers
    to annotate data-races around dst->dev").
    
    We want to gradually add explicit RCU protection to dst->dev,
    including lockdep support.
    
    Add an union to alias dst->dev_rcu and dst->dev.
    
    Add dst_dev_net_rcu() helper.
    
    Fixes: 4a6ce2b6f2ec ("net: introduce a new function dst_dev_put()")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Link: https://patch.msgid.link/20250828195823.3958522-2-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Gyokhan Kochmarla <gyokhan@amazon.com>
    
    [ Massaging patch to resolve rebase conflicts.
    This patch pulls in parts of
    88fe14253e1 net: dst: add four helpers to annotate data-races around dst->dev
    . The upstream patch doesn't apply cleanly so only the helpers
    are backported. So with this rebase, keep the helpers and the
    warn_once message. ]
    
    Signed-off-by: Stanislav Uschakow <suschako@amazon.de>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -1045,7 +1045,7 @@ static void __ip_rt_update_pmtu(struct rtable *rt, struct flowi4 *fl4, u32 mtu)
 		return;
 
 	rcu_read_lock();
-	net = dev_net_rcu(dst->dev);
+	net = dst_dev_net_rcu(dst);
 	if (mtu < net->ipv4.ip_rt_min_pmtu) {
 		lock = true;
 		mtu = min(old_mtu, net->ipv4.ip_rt_min_pmtu);
@@ -1343,8 +1343,14 @@ static void set_class_tag(struct rtable *rt, u32 tag)
 static unsigned int ipv4_default_advmss(const struct dst_entry *dst)
 {
 	unsigned int header_size = sizeof(struct tcphdr) + sizeof(struct iphdr);
-	unsigned int advmss = max_t(unsigned int, ipv4_mtu(dst) - header_size,
-				    ip_rt_min_advmss);
+	unsigned int advmss;
+	struct net *net;
+
+	rcu_read_lock();
+	net = dst_dev_net_rcu(dst);
+	advmss = max_t(unsigned int, ipv4_mtu(dst) - header_size,
+           ip_rt_min_advmss);
+	rcu_read_unlock();
 
 	return min(advmss, IPV4_MAX_PMTU - header_size);
 }
