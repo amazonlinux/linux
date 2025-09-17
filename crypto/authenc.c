ce7635ef8b8838163a970eba772ff542867e0c4d
Jay Wang <wanjay@amazon.com>
2025-09-17 05:35:03 +0000

    crypto: Add Crypto API wrapper for CONFIG_CRYPTO_AUTHENC
    Introduce DEFINE_CRYPTO_API_STUB() and DECLARE_CRYPTO_API()
    macros in the relevant header files, and add the corresponding
    implementation to fips140-api.c for CONFIG_CRYPTO_AUTHENC-related
    code.
    
    This patch is partially based on work by Vegard Nossum, with
    modifications. Unlike the original, we do not include
    DEFINE_CRYPTO_API since only one copy of the crypto symbols is
    kept, either in the crypto module or in the main kernel.
    
    Co-develop-by: Vegard Nossum <vegard.nossum@oracle.com>
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- crypto/authenc.c
+++ crypto/authenc.c
@@ -421,8 +421,8 @@ static void __exit crypto_authenc_module_exit(void)
 	crypto_unregister_template(&crypto_authenc_tmpl);
 }
 
-module_init(crypto_authenc_module_init);
-module_exit(crypto_authenc_module_exit);
+crypto_module_init(crypto_authenc_module_init);
+crypto_module_exit(crypto_authenc_module_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple AEAD wrapper for IPsec");
