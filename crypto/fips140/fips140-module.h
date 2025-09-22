e3625350aa2558951cfda5965bd51ca6b7cef453
Jay Wang <wanjay@amazon.com>
2025-09-22 21:50:28 +0000

    crypto: fips140: add module integrity self-check
    Add verify_integrity() function to perform HMAC-SHA256 self verification
    of this standalone crypto module against pre-computed hash. This integrity
    self-check is required by FIPS 140-3.
    
    This patch is picked from Vegard Nossum <vegard.nossum@oracle.com> with
    minor modifications.
    
    Co-developed-by: Vegard Nossum <vegard.nossum@oracle.com>
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- crypto/fips140/fips140-module.h
+++ crypto/fips140/fips140-module.h
@@ -9,9 +9,13 @@
 #include <linux/module.h>
 #include <linux/crypto.h>
 #include <crypto/algapi.h>
+#include <crypto/hash.h>
+#include <crypto/sha2.h>
 #include <linux/init.h>
+#include <linux/string.h>
 #include <linux/atomic.h>
 #include <linux/wait.h>
+#include <linux/fips.h>
 
 /* FIPS140 synchronization between kernel and module */
 extern atomic_t fips140_kernel_level_complete;
