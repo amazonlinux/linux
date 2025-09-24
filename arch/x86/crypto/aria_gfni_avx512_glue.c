a1dfe1e419dd237fe1340077300fc0d9dc89fc83
Jay Wang <wanjay@amazon.com>
2025-09-24 18:55:18 +0000

    x86: crypto: convert exported crypto symbol into pluggable interface for CONFIG_CRYPTO_ARIA_GFNI_AVX512_X86_64 crypto
    Apply Crypto API wrappers to the exported crypto symbol in
    CONFIG_CRYPTO_ARIA_GFNI_AVX512_X86_64-related crypto to convert them into pluggable
    interface.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- arch/x86/crypto/aria_gfni_avx512_glue.c
+++ arch/x86/crypto/aria_gfni_avx512_glue.c
@@ -232,8 +232,8 @@ static void __exit aria_avx512_exit(void)
 	crypto_unregister_skciphers(aria_algs, ARRAY_SIZE(aria_algs));
 }
 
-module_init(aria_avx512_init);
-module_exit(aria_avx512_exit);
+crypto_module_init(aria_avx512_init);
+crypto_module_exit(aria_avx512_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Taehee Yoo <ap420073@gmail.com>");
