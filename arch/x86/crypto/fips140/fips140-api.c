72f3b375b1516c13196f25447bdd41cb997ac1f9
Jay Wang <wanjay@amazon.com>
2025-09-24 18:55:18 +0000

    x86: crypto: Add Crypto API wrapper for CONFIG_CRYPTO_TWOFISH_X86_64_3WAY
    Introduce DEFINE_CRYPTO_API_STUB() and DECLARE_CRYPTO_API()
    macros in the relevant header files, and add the corresponding
    implementation to fips140-api.c for CONFIG_CRYPTO_TWOFISH_X86_64_3WAY-
    related code.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- arch/x86/crypto/fips140/fips140-api.c
+++ arch/x86/crypto/fips140/fips140-api.c
@@ -21,3 +21,16 @@ DEFINE_CRYPTO_API_STUB(twofish_enc_blk);
 DEFINE_CRYPTO_API_STUB(twofish_dec_blk);
 
 #endif
+
+/*
+ * arch/x86/crypto/twofish_glue_3way.c
+ */
+#if IS_BUILTIN(CONFIG_CRYPTO_TWOFISH_X86_64_3WAY)
+
+#include <arch/x86/crypto/twofish.h>
+
+DEFINE_CRYPTO_API_STUB(__twofish_enc_blk_3way);
+DEFINE_CRYPTO_API_STUB(twofish_dec_blk_3way);
+DEFINE_CRYPTO_API_STUB(twofish_dec_blk_cbc_3way);
+
+#endif
