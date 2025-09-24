c54bfe5ab4955563efce4a672dbf92b673bf9df5
Jay Wang <wanjay@amazon.com>
2025-09-24 18:55:18 +0000

    crypto: convert exported crypto symbol into pluggable interface for CONFIG_CRYPTO_TEA crypto
    Apply Crypto API wrappers to the exported crypto symbol in
    CONFIG_CRYPTO_TEA-related crypto to convert them into pluggable
    interface.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- crypto/tea.c
+++ crypto/tea.c
@@ -255,8 +255,8 @@ MODULE_ALIAS_CRYPTO("tea");
 MODULE_ALIAS_CRYPTO("xtea");
 MODULE_ALIAS_CRYPTO("xeta");
 
-module_init(tea_mod_init);
-module_exit(tea_mod_fini);
+crypto_module_init(tea_mod_init);
+crypto_module_exit(tea_mod_fini);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("TEA, XTEA & XETA Cryptographic Algorithms");
