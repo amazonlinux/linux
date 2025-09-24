dddf8bee0e55b55440095eefe1aa3d70bdfd2221
Jay Wang <wanjay@amazon.com>
2025-09-24 18:55:18 +0000

    crypto: convert exported crypto symbol into pluggable interface for CONFIG_CRYPTO_CAST_COMMON, CONFIG_CRYPTO_CAST5, CONFIG_CRYPTO_CAST6 crypto
    Apply Crypto API wrappers to the exported crypto symbol in
    CONFIG_CRYPTO_CAST_COMMON-, CONFIG_CRYPTO_CAST5-, and
    CONFIG_CRYPTO_CAST6-related crypto to convert them into pluggable
    interface.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- crypto/cast5_generic.c
+++ crypto/cast5_generic.c
@@ -531,8 +531,8 @@ static void __exit cast5_mod_fini(void)
 	crypto_unregister_alg(&alg);
 }
 
-module_init(cast5_mod_init);
-module_exit(cast5_mod_fini);
+crypto_module_init(cast5_mod_init);
+crypto_module_exit(cast5_mod_fini);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Cast5 Cipher Algorithm");
