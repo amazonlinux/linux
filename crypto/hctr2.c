99c6cbb0bed4170cacd9b6e5153ee3bff2b4e474
Jay Wang <wanjay@amazon.com>
2025-09-24 18:55:18 +0000

    crypto: Add Crypto API wrapper for CONFIG_CRYPTO_XCTR and CONFIG_CRYPTO_HCTR2
    Introduce DEFINE_CRYPTO_API_STUB() and DECLARE_CRYPTO_API()
    macros in the relevant header files, and add the corresponding
    implementation to fips140-api.c for CONFIG_CRYPTO_XCTR- and
    CONFIG_CRYPTO_HCTR2-related code.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- crypto/hctr2.c
+++ crypto/hctr2.c
@@ -570,8 +570,8 @@ static void __exit hctr2_module_exit(void)
 					   ARRAY_SIZE(hctr2_tmpls));
 }
 
-module_init(hctr2_module_init);
-module_exit(hctr2_module_exit);
+crypto_module_init(hctr2_module_init);
+crypto_module_exit(hctr2_module_exit);
 
 MODULE_DESCRIPTION("HCTR2 length-preserving encryption mode");
 MODULE_LICENSE("GPL v2");
