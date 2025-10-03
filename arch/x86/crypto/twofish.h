1456dee5ca4185a529d1720907ae4ad79dd17d69
Jay Wang <wanjay@amazon.com>
2025-10-03 06:43:09 +0000

    CONFIG_CRYPTO_TWOFISH_X86_64_3WAY

--- arch/x86/crypto/twofish.h
+++ arch/x86/crypto/twofish.h
@@ -16,15 +16,15 @@ DECLARE_CRYPTO_API(CONFIG_CRYPTO_TWOFISH_X86_64, twofish_dec_blk, asmlinkage voi
 	(ctx, dst, src));
 
 /* 3-way parallel cipher functions */
-DECLARE_CRYPTO_API(__twofish_enc_blk_3way, asmlinkage void,
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_TWOFISH_X86_64_3WAY, __twofish_enc_blk_3way, asmlinkage void,
 	(const void *ctx, u8 *dst, const u8 *src, bool xor),
 	(ctx, dst, src, xor));
-DECLARE_CRYPTO_API(twofish_dec_blk_3way, asmlinkage void,
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_TWOFISH_X86_64_3WAY, twofish_dec_blk_3way, asmlinkage void,
 	(const void *ctx, u8 *dst, const u8 *src),
 	(ctx, dst, src));
 
 /* helpers from twofish_x86_64-3way module */
-DECLARE_CRYPTO_API(twofish_dec_blk_cbc_3way, void,
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_TWOFISH_X86_64_3WAY, twofish_dec_blk_cbc_3way, void,
 	(const void *ctx, u8 *dst, const u8 *src),
 	(ctx, dst, src));
 
