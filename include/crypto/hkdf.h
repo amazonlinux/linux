10d520833159e2c34852b472f175924f3aef41e3
Jay Wang <wanjay@amazon.com>
2025-10-03 05:40:40 +0000

    CONFIG_CRYPTO_HKDF

--- include/crypto/hkdf.h
+++ include/crypto/hkdf.h
@@ -12,10 +12,10 @@
 #include <crypto/api.h>
 #include <crypto/hash.h>
 
-DECLARE_CRYPTO_API(hkdf_extract, int,
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_HKDF, hkdf_extract, int,
 	(struct crypto_shash *hmac_tfm, const u8 *ikm, unsigned int ikmlen, const u8 *salt, unsigned int saltlen, u8 *prk),
 	(hmac_tfm, ikm, ikmlen, salt, saltlen, prk));
-DECLARE_CRYPTO_API(hkdf_expand, int,
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_HKDF, hkdf_expand, int,
 	(struct crypto_shash *hmac_tfm, const u8 *info, unsigned int infolen, u8 *okm, unsigned int okmlen),
 	(hmac_tfm, info, infolen, okm, okmlen));
 #endif
