d8ef4bd0167297780c60d90f0b9d4684db8faaf0
Jay Wang <wanjay@amazon.com>
2025-10-03 05:56:21 +0000

    CONFIG_CRYPTO_SM4

--- include/crypto/sm4.h
+++ include/crypto/sm4.h
@@ -22,11 +22,11 @@ struct sm4_ctx {
 	u32 rkey_dec[SM4_RKEY_WORDS];
 };
 
-DECLARE_CRYPTO_VAR(crypto_sm4_fk, const u32, [4]);
-DECLARE_CRYPTO_VAR(crypto_sm4_ck, const u32, [32]);
-DECLARE_CRYPTO_VAR(crypto_sm4_sbox, const u8, [256]);
+DECLARE_CRYPTO_VAR(CONFIG_CRYPTO_SM4, crypto_sm4_fk, const u32, [4]);
+DECLARE_CRYPTO_VAR(CONFIG_CRYPTO_SM4, crypto_sm4_ck, const u32, [32]);
+DECLARE_CRYPTO_VAR(CONFIG_CRYPTO_SM4, crypto_sm4_sbox, const u8, [256]);
 
-#if defined(CONFIG_CRYPTO_FIPS140_EXTMOD) && !defined(FIPS_MODULE)
+#if defined(CONFIG_CRYPTO_FIPS140_EXTMOD) && !defined(FIPS_MODULE) && IS_BUILTIN(CONFIG_CRYPTO_SM4)
 #define crypto_sm4_fk (((const u32*)CRYPTO_VAR_NAME(crypto_sm4_fk)))
 #define crypto_sm4_ck (((const u32*)CRYPTO_VAR_NAME(crypto_sm4_ck)))
 #define crypto_sm4_sbox (((const u8*)CRYPTO_VAR_NAME(crypto_sm4_sbox)))
@@ -41,7 +41,7 @@ DECLARE_CRYPTO_VAR(crypto_sm4_sbox, const u8, [256]);
  * Returns 0 on success. The function fails only if an invalid key size (or
  * pointer) is supplied.
  */
-DECLARE_CRYPTO_API(sm4_expandkey, int,
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_SM4, sm4_expandkey, int,
 	(struct sm4_ctx *ctx, const u8 *in_key, unsigned int key_len),
 	(ctx, in_key, key_len));
 
@@ -51,7 +51,7 @@ DECLARE_CRYPTO_API(sm4_expandkey, int,
  * @out:	Buffer to store output data
  * @in: 	Buffer containing the input data
  */
-DECLARE_CRYPTO_API(sm4_crypt_block, void,
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_SM4, sm4_crypt_block, void,
 	(const u32 *rk, u8 *out, const u8 *in),
 	(rk, out, in));
 
