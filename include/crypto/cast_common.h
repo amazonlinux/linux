33c2846ba6db12edd7b9bad5bcb2307dcbe9dc20
Jay Wang <wanjay@amazon.com>
2025-10-03 06:01:30 +0000

    CONFIG_CRYPTO_CAST_COMMON

--- include/crypto/cast_common.h
+++ include/crypto/cast_common.h
@@ -4,12 +4,12 @@
 
 #include <crypto/api.h>
 
-DECLARE_CRYPTO_VAR(cast_s1, const u32, [256]);
-DECLARE_CRYPTO_VAR(cast_s2, const u32, [256]);
-DECLARE_CRYPTO_VAR(cast_s3, const u32, [256]);
-DECLARE_CRYPTO_VAR(cast_s4, const u32, [256]);
+DECLARE_CRYPTO_VAR(CONFIG_CRYPTO_CAST_COMMON, cast_s1, const u32, [256]);
+DECLARE_CRYPTO_VAR(CONFIG_CRYPTO_CAST_COMMON, cast_s2, const u32, [256]);
+DECLARE_CRYPTO_VAR(CONFIG_CRYPTO_CAST_COMMON, cast_s3, const u32, [256]);
+DECLARE_CRYPTO_VAR(CONFIG_CRYPTO_CAST_COMMON, cast_s4, const u32, [256]);
 
-#if defined(CONFIG_CRYPTO_FIPS140_EXTMOD) && !defined(FIPS_MODULE)
+#if defined(CONFIG_CRYPTO_FIPS140_EXTMOD) && !defined(FIPS_MODULE) && IS_BUILTIN(CONFIG_CRYPTO_CAST_COMMON)
 #define cast_s1 (((const u32*)CRYPTO_VAR_NAME(cast_s1)))
 #define cast_s2 (((const u32*)CRYPTO_VAR_NAME(cast_s2)))
 #define cast_s3 (((const u32*)CRYPTO_VAR_NAME(cast_s3)))
