555e777a957b0e20d567cdd64e9c7bf3fdcbf1cc
Jay Wang <wanjay@amazon.com>
2025-10-17 01:33:36 +0000

    arm64: crypto: convert exported crypto symbol into pluggable interface for CONFIG_CRYPTO_AES_ARM64_CE crypto
    Apply Crypto API wrappers to the exported crypto symbol in
    CONFIG_CRYPTO_AES_ARM64_CE-related crypto to convert them into pluggable
    interface.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- arch/arm64/crypto/fips140/fips140-api.c
+++ arch/arm64/crypto/fips140/fips140-api.c
@@ -10,3 +10,15 @@ DEFINE_CRYPTO_API_STUB(sm4_ce_crypt_block);
 DEFINE_CRYPTO_API_STUB(sm4_ce_cbc_enc);
 
 #endif
+
+/*
+ * arch/arm64/crypto/aes-ce-glue.c
+ */
+#if IS_BUILTIN(CONFIG_CRYPTO_AES_ARM64_CE)
+
+#include <arch/arm64/crypto/aes-ce-setkey.h>
+
+DEFINE_CRYPTO_API_STUB(ce_aes_setkey);
+DEFINE_CRYPTO_API_STUB(ce_aes_expandkey);
+
+#endif
