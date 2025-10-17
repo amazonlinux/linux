6276442b5db3a0110cf78e1bdb61ea19c5abfb65
Jay Wang <wanjay@amazon.com>
2025-10-17 01:33:36 +0000

    arm64: crypto: convert exported crypto symbol into pluggable interface for CONFIG_CRYPTO_AES_ARM64_CE crypto
    Apply Crypto API wrappers to the exported crypto symbol in
    CONFIG_CRYPTO_AES_ARM64_CE-related crypto to convert them into pluggable
    interface.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- arch/arm64/crypto/aes-ce-setkey.h
+++ arch/arm64/crypto/aes-ce-setkey.h
@@ -1,6 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
-int ce_aes_setkey(struct crypto_tfm *tfm, const u8 *in_key,
-		  unsigned int key_len);
-int ce_aes_expandkey(struct crypto_aes_ctx *ctx, const u8 *in_key,
-		     unsigned int key_len);
+#include <crypto/api.h>
+#include <crypto/aes.h>
+
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_AES_ARM64_CE, ce_aes_setkey, int,
+	(struct crypto_tfm *tfm, const u8 *in_key, unsigned int key_len),
+	(tfm, in_key, key_len));
+
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_AES_ARM64_CE, ce_aes_expandkey, int,
+	(struct crypto_aes_ctx *ctx, const u8 *in_key, unsigned int key_len),
+	(ctx, in_key, key_len));
