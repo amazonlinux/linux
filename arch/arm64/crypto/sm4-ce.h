8e6a96a8b5164feff7d1e770df4aabbe63d1073d
Jay Wang <wanjay@amazon.com>
2025-10-17 01:09:50 +0000

    arm64: crypto: convert exported crypto symbol into pluggable interface for CONFIG_CRYPTO_SM4_ARM64_CE_BLK crypto
    Apply Crypto API wrappers to the exported crypto symbol in
    CONFIG_CRYPTO_SM4_ARM64_CE_BLK-related crypto to convert them into pluggable
    interface.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- arch/arm64/crypto/sm4-ce.h
+++ arch/arm64/crypto/sm4-ce.h
@@ -4,10 +4,16 @@
  * Copyright (C) 2022 Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
  */
 
-void sm4_ce_expand_key(const u8 *key, u32 *rkey_enc, u32 *rkey_dec,
-		       const u32 *fk, const u32 *ck);
+#include <crypto/api.h>
 
-void sm4_ce_crypt_block(const u32 *rkey, u8 *dst, const u8 *src);
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_SM4_ARM64_CE_BLK, sm4_ce_expand_key, void,
+	(const u8 *key, u32 *rkey_enc, u32 *rkey_dec, const u32 *fk, const u32 *ck),
+	(key, rkey_enc, rkey_dec, fk, ck));
 
-void sm4_ce_cbc_enc(const u32 *rkey_enc, u8 *dst, const u8 *src,
-		    u8 *iv, unsigned int nblocks);
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_SM4_ARM64_CE_BLK, sm4_ce_crypt_block, void,
+	(const u32 *rkey, u8 *dst, const u8 *src),
+	(rkey, dst, src));
+
+DECLARE_CRYPTO_API(CONFIG_CRYPTO_SM4_ARM64_CE_BLK, sm4_ce_cbc_enc, void,
+	(const u32 *rkey_enc, u8 *dst, const u8 *src, u8 *iv, unsigned int nblocks),
+	(rkey_enc, dst, src, iv, nblocks));
