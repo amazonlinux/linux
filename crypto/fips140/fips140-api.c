074793132eef73decb2bdd71f6d81d88e1956126
Jay Wang <wanjay@amazon.com>
2025-11-06 07:15:11 +0000

    crypto: add pluggable interface for builtin crypto modules
    Traditionally, builtin cryptos interact with the main kernel through
    exported functions and variables via EXPORT_SYMBOL. The exported
    symbols are statically tied with the main kernel, so not suitable
    after those builtin cryptos moved to a standalone kernel module
    (e.g., main kernel will not even be compilable since users need the
    exact function/variable address).
    
    To address this, introduce a pluggable interface between builtin
    crypto functions and variables by placing address placeholders.
    During runtime once the crypto kernel module is loaded, the address
    placeholder gets updated (by `do_crypto_api` and `do_crypto_var`) to
    correct address.
    
    In more details, there are two types of address holders: for function
    addresses, "static call" mechanism is used; for variable addresses, a
    var_type* pointer is used. To apply this pluggable interface, just
    wrap the function/variable header declaration with the placeholder
    declaration wrappers (implemented as `DECLARE_STATIC_CALL()`/
    `DECLARE_CRYPTO_VAR()`), and place the placeholder definition wrappers
    for each crypto function/variable in fips140-api.c (implemented as
    `DEFINE_CRYPTO_API_STUB()`/`DEFINE_CRYPTO_VAR_STUB()`). Those wrappers
    will be compiled differently for main kernel and the crypto kernel
    module using different compilation flags. To make such pluggable
    interface only affect cryptos chosen as builtin but not crypto chosen
    built as modules already, associate wrappers with CONFIG_CRYPTO_{NAME}
    parameter.
    
    In addition to the pluggable interface, this patch also adds a way to
    collect crypto init functions, such as `module_init()` into a
    dedicated ELF section for later module entry to initialize each
    crypto.
    
    The idea of using "static call" as pluggable interface for function
    address stems from Vegard Nossum <vegard.nossum@oracle.com>, while
    we additionally make the following core differences: avoid duplicate
    crypto code in main kernel, allow variable pluggable, make this
    feature not affect cryptos already chosen to be module-built, and
    make this adapt to any .config choices.
    
    Signed-off-by: Jay Wang <wanjay@amazon.com>

---
+++ crypto/fips140/fips140-api.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+/*
+ * Define static call keys for any functions which are part of the crypto
+ * API and used by the standalone FIPS module but which are not built into
+ * vmlinux.
+ */
