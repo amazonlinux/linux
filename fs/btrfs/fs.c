88ee10b708f7c99d884ae667ddc5d83f1e97c5fa
Eric Biggers <ebiggers@kernel.org>
2025-12-04 23:04:54 -0800

    btrfs: switch to library APIs for checksums
    Make btrfs use the library APIs instead of crypto_shash, for all
    checksum computations.  This has many benefits:
    
    - Allows future checksum types, e.g. XXH3 or CRC64, to be more easily
      supported.  Only a library API will be needed, not crypto_shash too.
    
    - Eliminates the overhead of the generic crypto layer, including an
      indirect call for every function call and other API overhead.  A
      microbenchmark of btrfs_check_read_bio() with crc32c checksums shows a
      speedup from 658 cycles to 608 cycles per 4096-byte block.
    
    - Decreases the stack usage of btrfs by reducing the size of checksum
      contexts from 384 bytes to 240 bytes, and by eliminating the need for
      some functions to declare a checksum context at all.
    
    - Increases reliability.  The library functions always succeed and
      return void.  In contrast, crypto_shash can fail and return errors.
      Also, the library functions are guaranteed to be available when btrfs
      is loaded; there's no longer any need to use module softdeps to try to
      work around the crypto modules sometimes not being loaded.
    
    - Fixes a bug where blake2b checksums didn't work on kernels booted with
      fips=1.  Since btrfs checksums are for integrity only, it's fine for
      them to use non-FIPS-approved algorithms.
    
    Note that with having to handle 4 algorithms instead of just 1-2, this
    commit does result in a slightly positive diffstat.  That being said,
    this wouldn't have been the case if btrfs had actually checked for
    errors from crypto_shash, which technically it should have been doing.
    
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Neal Gompa <neal@gompa.dev>
    Signed-off-by: Eric Biggers <ebiggers@kernel.org>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    [Implements self-contained blake2b for 6.18 as lib/crypto/blake2b.c doesn't
    exist yet; uses sha256_ctx API and places csum functions in fs.c/h.]
    Signed-off-by: Jay Wang <wanjay@amazon.com>

--- fs/btrfs/fs.c
+++ fs/btrfs/fs.c
@@ -1,20 +1,132 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#include <linux/crc32c.h>
+#include <linux/xxhash.h>
+#include <crypto/sha2.h>
+#include <crypto/blake2b.h>
+#include <linux/unaligned.h>
 #include "messages.h"
 #include "fs.h"
 #include "accessors.h"
 #include "volumes.h"
 
+/* Minimal blake2b implementation for btrfs checksums */
+static const u64 blake2b_iv[8] = {
+	0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
+	0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
+	0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
+	0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
+};
+
+static const u8 blake2b_sigma[12][16] = {
+	{  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 },
+	{ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 },
+	{ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 },
+	{  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 },
+	{  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 },
+	{  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 },
+	{ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 },
+	{ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 },
+	{  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 },
+	{ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 },
+	{  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 },
+	{ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 }
+};
+
+static void blake2b_compress(struct blake2b_state *S, const u8 *block)
+{
+	u64 m[16], v[16];
+	int i;
+
+	for (i = 0; i < 16; i++)
+		m[i] = get_unaligned_le64(block + i * 8);
+
+	for (i = 0; i < 8; i++)
+		v[i] = S->h[i];
+	v[ 8] = blake2b_iv[0];
+	v[ 9] = blake2b_iv[1];
+	v[10] = blake2b_iv[2];
+	v[11] = blake2b_iv[3];
+	v[12] = S->t[0] ^ blake2b_iv[4];
+	v[13] = S->t[1] ^ blake2b_iv[5];
+	v[14] = S->f[0] ^ blake2b_iv[6];
+	v[15] = S->f[1] ^ blake2b_iv[7];
+
+#define G(r,i,a,b,c,d) \
+	do { \
+		a = a + b + m[blake2b_sigma[r][2*i+0]]; \
+		d = ror64(d ^ a, 32); \
+		c = c + d; \
+		b = ror64(b ^ c, 24); \
+		a = a + b + m[blake2b_sigma[r][2*i+1]]; \
+		d = ror64(d ^ a, 16); \
+		c = c + d; \
+		b = ror64(b ^ c, 63); \
+	} while (0)
+
+#define ROUND(r) \
+	do { \
+		G(r,0,v[ 0],v[ 4],v[ 8],v[12]); \
+		G(r,1,v[ 1],v[ 5],v[ 9],v[13]); \
+		G(r,2,v[ 2],v[ 6],v[10],v[14]); \
+		G(r,3,v[ 3],v[ 7],v[11],v[15]); \
+		G(r,4,v[ 0],v[ 5],v[10],v[15]); \
+		G(r,5,v[ 1],v[ 6],v[11],v[12]); \
+		G(r,6,v[ 2],v[ 7],v[ 8],v[13]); \
+		G(r,7,v[ 3],v[ 4],v[ 9],v[14]); \
+	} while (0)
+
+	ROUND(0); ROUND(1); ROUND(2); ROUND(3);
+	ROUND(4); ROUND(5); ROUND(6); ROUND(7);
+	ROUND(8); ROUND(9); ROUND(10); ROUND(11);
+
+#undef G
+#undef ROUND
+
+	for (i = 0; i < 8; i++)
+		S->h[i] ^= v[i] ^ v[i + 8];
+}
+
+static void blake2b_increment_counter(struct blake2b_state *S, u64 inc)
+{
+	S->t[0] += inc;
+	S->t[1] += (S->t[0] < inc);
+}
+
+static void blake2b_256(const u8 *data, size_t len, u8 *out)
+{
+	struct blake2b_state S;
+	u8 buf[128] = {0};
+	size_t i;
+
+	__blake2b_init(&S, 32, 0);
+	S.t[0] = S.t[1] = 0;
+	S.f[0] = S.f[1] = 0;
+
+	while (len > 128) {
+		blake2b_increment_counter(&S, 128);
+		blake2b_compress(&S, data);
+		data += 128;
+		len -= 128;
+	}
+
+	blake2b_increment_counter(&S, len);
+	S.f[0] = -1;
+	memcpy(buf, data, len);
+	blake2b_compress(&S, buf);
+
+	for (i = 0; i < 4; i++)
+		put_unaligned_le64(S.h[i], out + i * 8);
+}
+
 static const struct btrfs_csums {
 	u16		size;
 	const char	name[10];
-	const char	driver[12];
 } btrfs_csums[] = {
 	[BTRFS_CSUM_TYPE_CRC32] = { .size = 4, .name = "crc32c" },
 	[BTRFS_CSUM_TYPE_XXHASH] = { .size = 8, .name = "xxhash64" },
 	[BTRFS_CSUM_TYPE_SHA256] = { .size = 32, .name = "sha256" },
-	[BTRFS_CSUM_TYPE_BLAKE2] = { .size = 32, .name = "blake2b",
-				     .driver = "blake2b-256" },
+	[BTRFS_CSUM_TYPE_BLAKE2] = { .size = 32, .name = "blake2b" },
 };
 
 /* This exists for btrfs-progs usages. */
@@ -37,21 +149,135 @@ const char *btrfs_super_csum_name(u16 csum_type)
 	return btrfs_csums[csum_type].name;
 }
 
-/*
- * Return driver name if defined, otherwise the name that's also a valid driver
- * name.
- */
-const char *btrfs_super_csum_driver(u16 csum_type)
-{
-	/* csum type is validated at mount time */
-	return btrfs_csums[csum_type].driver[0] ?
-		btrfs_csums[csum_type].driver :
-		btrfs_csums[csum_type].name;
-}
-
 size_t __attribute_const__ btrfs_get_num_csums(void)
 {
 	return ARRAY_SIZE(btrfs_csums);
+}
+
+void btrfs_csum(u16 csum_type, const u8 *data, size_t len, u8 *out)
+{
+	switch (csum_type) {
+	case BTRFS_CSUM_TYPE_CRC32:
+		put_unaligned_le32(~crc32c(~0, data, len), out);
+		break;
+	case BTRFS_CSUM_TYPE_XXHASH:
+		put_unaligned_le64(xxh64(data, len, 0), out);
+		break;
+	case BTRFS_CSUM_TYPE_SHA256:
+		sha256(data, len, out);
+		break;
+	case BTRFS_CSUM_TYPE_BLAKE2:
+		blake2b_256(data, len, out);
+		break;
+	default:
+		/* Checksum type is validated at mount time. */
+		BUG();
+	}
+}
+
+void btrfs_csum_init(struct btrfs_csum_ctx *ctx, u16 csum_type)
+{
+	ctx->csum_type = csum_type;
+	switch (ctx->csum_type) {
+	case BTRFS_CSUM_TYPE_CRC32:
+		ctx->crc32 = ~0;
+		break;
+	case BTRFS_CSUM_TYPE_XXHASH:
+		xxh64_reset(&ctx->xxh64, 0);
+		break;
+	case BTRFS_CSUM_TYPE_SHA256:
+		sha256_init(&ctx->sha256);
+		break;
+	case BTRFS_CSUM_TYPE_BLAKE2:
+		__blake2b_init(&ctx->blake2b, 32, 0);
+		ctx->blake2b.t[0] = ctx->blake2b.t[1] = 0;
+		ctx->blake2b.f[0] = ctx->blake2b.f[1] = 0;
+		ctx->buflen = 0;
+		break;
+	default:
+		/* Checksum type is validated at mount time. */
+		BUG();
+	}
+}
+
+void btrfs_csum_update(struct btrfs_csum_ctx *ctx, const u8 *data, size_t len)
+{
+	switch (ctx->csum_type) {
+	case BTRFS_CSUM_TYPE_CRC32:
+		ctx->crc32 = crc32c(ctx->crc32, data, len);
+		break;
+	case BTRFS_CSUM_TYPE_XXHASH:
+		xxh64_update(&ctx->xxh64, data, len);
+		break;
+	case BTRFS_CSUM_TYPE_SHA256:
+		sha256_update(&ctx->sha256, data, len);
+		break;
+	case BTRFS_CSUM_TYPE_BLAKE2: {
+		size_t offset = 0;
+		
+		/* Fill buffer if we have partial data */
+		if (ctx->buflen > 0) {
+			size_t to_copy = min(128 - ctx->buflen, len);
+			memcpy(ctx->buf + ctx->buflen, data, to_copy);
+			ctx->buflen += to_copy;
+			offset += to_copy;
+			
+			if (ctx->buflen == 128) {
+				blake2b_increment_counter(&ctx->blake2b, 128);
+				blake2b_compress(&ctx->blake2b, ctx->buf);
+				ctx->buflen = 0;
+			}
+		}
+		
+		/* Process full blocks */
+		while (offset + 128 <= len) {
+			blake2b_increment_counter(&ctx->blake2b, 128);
+			blake2b_compress(&ctx->blake2b, data + offset);
+			offset += 128;
+		}
+		
+		/* Buffer remaining data */
+		if (offset < len) {
+			memcpy(ctx->buf, data + offset, len - offset);
+			ctx->buflen = len - offset;
+		}
+		break;
+	}
+	default:
+		/* Checksum type is validated at mount time. */
+		BUG();
+	}
+}
+
+void btrfs_csum_final(struct btrfs_csum_ctx *ctx, u8 *out)
+{
+	switch (ctx->csum_type) {
+	case BTRFS_CSUM_TYPE_CRC32:
+		put_unaligned_le32(~ctx->crc32, out);
+		break;
+	case BTRFS_CSUM_TYPE_XXHASH:
+		put_unaligned_le64(xxh64_digest(&ctx->xxh64), out);
+		break;
+	case BTRFS_CSUM_TYPE_SHA256:
+		sha256_final(&ctx->sha256, out);
+		break;
+	case BTRFS_CSUM_TYPE_BLAKE2: {
+		u8 buf[128] = {0};
+		int i;
+		
+		blake2b_increment_counter(&ctx->blake2b, ctx->buflen);
+		ctx->blake2b.f[0] = -1;
+		memcpy(buf, ctx->buf, ctx->buflen);
+		blake2b_compress(&ctx->blake2b, buf);
+		
+		for (i = 0; i < 4; i++)
+			put_unaligned_le64(ctx->blake2b.h[i], out + i * 8);
+		break;
+	}
+	default:
+		/* Checksum type is validated at mount time. */
+		BUG();
+	}
 }
 
 /*
