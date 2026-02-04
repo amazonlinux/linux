b1ff13b15497079f01a43d9fa4ed4b2f781345b8
Suraj Jitindar Singh <surajjs@amazon.com>
2026-02-04 22:00:01 +0000

    KVM/arm64: Only write TCR_EL1 and VTCR_EL2 if value differs
    It is not necessary to perform the actual register write if the value
    does not differ from the current value, so optimise in this case.
    
    Signed-off-by: Suraj Jitindar Singh <surajjs@amazon.com>

--- arch/arm64/include/asm/kvm_mmu.h
+++ arch/arm64/include/asm/kvm_mmu.h
@@ -274,7 +274,8 @@ static __always_inline u64 kvm_get_vttbr(struct kvm_s2_mmu *mmu)
  */
 static __always_inline void __load_guest_stage2(struct kvm_s2_mmu *mmu)
 {
-	write_sysreg(kern_hyp_va(mmu->kvm)->arch.vtcr, vtcr_el2);
+	// Only restore register if the value has changed.
+	write_sysreg_if_changed(kern_hyp_va(mmu->kvm)->arch.vtcr, vtcr_el2);
 	write_sysreg(kvm_get_vttbr(mmu), vttbr_el2);
 
 	/*
