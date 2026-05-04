c2afea09f608e19c25f4204c5832dbe2866107aa
EC2 Default User <ec2-user@ip-172-31-75-28.ec2.internal>
2026-05-04 22:01:54 +0000

    AL2023 6.12: Update lustrefsx to 2.15.6-fsx30 commit

--- fs/lustre/undef.h
+++ fs/lustre/undef.h
@@ -750,6 +750,9 @@
 /* 'ktime_to_timespec64' is available */
 #undef HAVE_KTIME_TO_TIMESPEC64
 
+/* lbug_with_loc is in objtool global_noreturns array */
+#undef HAVE_LBUG_WITH_LOC_IN_OBJTOOL
+
 /* ldiskfsfs_dirhash takes an inode argument */
 #undef HAVE_LDISKFSFS_GETHASH_INODE_ARG
 
