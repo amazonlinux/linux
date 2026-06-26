14f4f3d7d1d6b8b7f0d0b8170706cc3c39a86509
Stanislav Uschakow <suschako@amazon.de>
2026-06-26 13:17:33 +0200

    netfilter: nf_tables: use list_del_rcu for netlink hooks
    [Upstream commit f3224ee463f8f6f6ced7dcdf6081add4f8128527]
    
    nft_netdev_unregister_hooks and __nft_unregister_flowtable_net_hooks need
    to use list_del_rcu(), this list can be walked by concurrent dumpers.
    
    Add a new helper and use it consistently.
    
    Fixes: f9a43007d3f7 ("netfilter: nf_tables: double hook unregistration in netns path")
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    [Adapted for 6.1: The 6.1 tree uses a single hook->ops and kfree_rcu(hook, rcu)
    directly, rather than the ops_list and nft_netdev_hook_free_rcu() helper present
    in later kernels. The new nft_netdev_hook_unlink_free_rcu() helper was adapted
    to call kfree_rcu() directly. The nf_tables_updchain err_hooks comment was not
    applicable as 6.1 uses nft_chain_release_hook() for that path instead.]
    
    Signed-off-by: Stanislav Uschakow <suschako@amazon.de>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -301,6 +301,12 @@ err_register:
 	return err;
 }
 
+static void nft_netdev_hook_unlink_free_rcu(struct nft_hook *hook)
+{
+	list_del_rcu(&hook->list);
+	kfree_rcu(hook, rcu);
+}
+
 static void nft_netdev_unregister_hooks(struct net *net,
 					struct list_head *hook_list,
 					bool release_netdev)
@@ -309,10 +315,8 @@ static void nft_netdev_unregister_hooks(struct net *net,
 
 	list_for_each_entry_safe(hook, next, hook_list, list) {
 		nf_unregister_net_hook(net, &hook->ops);
-		if (release_netdev) {
-			list_del(&hook->list);
-			kfree_rcu(hook, rcu);
-		}
+		if (release_netdev)
+			nft_netdev_hook_unlink_free_rcu(hook);
 	}
 }
 
@@ -2056,10 +2060,8 @@ void nf_tables_chain_destroy(struct nft_chain *chain)
 
 		if (nft_base_chain_netdev(table->family, basechain->ops.hooknum)) {
 			list_for_each_entry_safe(hook, next,
-						 &basechain->hook_list, list) {
-				list_del_rcu(&hook->list);
-				kfree_rcu(hook, rcu);
-			}
+						 &basechain->hook_list, list)
+				nft_netdev_hook_unlink_free_rcu(hook);
 		}
 		module_put(basechain->type->owner);
 		if (rcu_access_pointer(basechain->stats)) {
@@ -8077,10 +8079,8 @@ static void __nft_unregister_flowtable_net_hooks(struct net *net,
 		nf_unregister_net_hook(net, &hook->ops);
 		flowtable->data.type->setup(&flowtable->data, hook->ops.dev,
 					    FLOW_BLOCK_UNBIND);
-		if (release_netdev) {
-			list_del(&hook->list);
-			kfree_rcu(hook, rcu);
-		}
+		if (release_netdev)
+			nft_netdev_hook_unlink_free_rcu(hook);
 	}
 }
 
@@ -8139,8 +8139,7 @@ err_unregister_net_hooks:
 			break;
 
 		nft_unregister_flowtable_hook(net, flowtable, hook);
-		list_del_rcu(&hook->list);
-		kfree_rcu(hook, rcu);
+		nft_netdev_hook_unlink_free_rcu(hook);
 	}
 
 	return err;
@@ -8150,10 +8149,8 @@ static void nft_hooks_destroy(struct list_head *hook_list)
 {
 	struct nft_hook *hook, *next;
 
-	list_for_each_entry_safe(hook, next, hook_list, list) {
-		list_del_rcu(&hook->list);
-		kfree_rcu(hook, rcu);
-	}
+	list_for_each_entry_safe(hook, next, hook_list, list)
+		nft_netdev_hook_unlink_free_rcu(hook);
 }
 
 static int nft_flowtable_update(struct nft_ctx *ctx, const struct nlmsghdr *nlh,
@@ -8236,8 +8233,7 @@ err_flowtable_update_hook:
 	list_for_each_entry_safe(hook, next, &flowtable_hook.list, list) {
 		if (unregister)
 			nft_unregister_flowtable_hook(ctx->net, flowtable, hook);
-		list_del_rcu(&hook->list);
-		kfree_rcu(hook, rcu);
+		nft_netdev_hook_unlink_free_rcu(hook);
 	}
 
 	return err;
@@ -8722,13 +8718,8 @@ err:
 
 static void nf_tables_flowtable_destroy(struct nft_flowtable *flowtable)
 {
-	struct nft_hook *hook, *next;
-
 	flowtable->data.type->free(&flowtable->data);
-	list_for_each_entry_safe(hook, next, &flowtable->hook_list, list) {
-		list_del_rcu(&hook->list);
-		kfree_rcu(hook, rcu);
-	}
+	nft_hooks_destroy(&flowtable->hook_list);
 	kfree(flowtable->name);
 	module_put(flowtable->data.type->owner);
 	kfree(flowtable);
