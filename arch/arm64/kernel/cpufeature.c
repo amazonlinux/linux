ekuiter/torte-linux

arm64: Add HAS_ECV_CNTPOFF capability
Add the probing code for the FEAT_ECV variant that implements CNTPOFF_EL2.
Why it is optional is a mystery, but let's try and detect it.

Reviewed-by: Reiji Watanabe <reijiw@google.com>
Reviewed-by: Colton Lewis <coltonlewis@google.com>
Signed-off-by: Marc Zyngier <maz@kernel.org>
Link: https://lore.kernel.org/r/20230330174800.2677007-4-maz@kernel.org

arch/arm64/kernel/cpufeature.c
@@ -2223,6 +2223,17 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.sign = FTR_UNSIGNED,
 		.min_field_value = 1,
 	},
+	{
+		.desc = "Enhanced Counter Virtualization (CNTPOFF)",
+		.capability = ARM64_HAS_ECV_CNTPOFF,
+		.type = ARM64_CPUCAP_SYSTEM_FEATURE,
+		.matches = has_cpuid_feature,
+		.sys_reg = SYS_ID_AA64MMFR0_EL1,
+		.field_pos = ID_AA64MMFR0_EL1_ECV_SHIFT,
+		.field_width = 4,
+		.sign = FTR_UNSIGNED,
+		.min_field_value = ID_AA64MMFR0_EL1_ECV_CNTPOFF,
+	},
 #ifdef CONFIG_ARM64_PAN
 	{
 		.desc = "Privileged Access Never",

amazonlinux/linux

[Backport from 326349943ed1 to 5.10. Made changes to use 5.10's approach
adding cpu feature. Replaced ID_AA64MMFR0_EL1_ECV_SHIFT with
ID_AA64MMFR0_ECV_SHIFT which are the same macro but changed name in
6.1[1]. Added ID_AA64MMFR0_ECV_CNTPOFF macro to sysreg.h, which has the
same defination as later added ID_AA64MMFR0_EL1_ECV_CNTPOFF[2]. Removed
field_width since it didn't exist in 5.10 and it was default as 4[3].]
[1] https://lore.kernel.org/r/20220905225425.1871461-5-broonie@kernel.org
[2] The current apporach adding a sysreg is through a generating script plus
    a config file. By manually run the script, we can find out what is
    ID_AA64MMFR0_EL1_ECV_CNTPOFF's real defination.
    e.g.:
    ./gen-sysreg.awk ./sysreg | grep ID_AA64MMFR0_EL1_ECV_CNTPOFF
    #define ID_AA64MMFR0_EL1_ECV_CNTPOFF            UL(0b0010)
    Hence, define ID_AA64MMFR0_ECV_CNTPOFF as 0x2 here.
[3] https://lore.kernel.org/r/20220207152109.197566-4-broonie@kernel.org
Signed-off-by: Nathan Gao <zcgao@amazon.com>

arch/arm64/kernel/cpufeature.c
@@ -1832,6 +1832,16 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.sign = FTR_UNSIGNED,
 		.min_field_value = 1,
 	},
+	{
+		.desc = "Enhanced Counter Virtualization (CNTPOFF)",
+		.capability = ARM64_HAS_ECV_CNTPOFF,
+		.type = ARM64_CPUCAP_SYSTEM_FEATURE,
+		.matches = has_cpuid_feature,
+		.sys_reg = SYS_ID_AA64MMFR0_EL1,
+		.field_pos = ID_AA64MMFR0_ECV_SHIFT,
+		.sign = FTR_UNSIGNED,
+		.min_field_value = ID_AA64MMFR0_ECV_CNTPOFF,
+	},
 #ifdef CONFIG_ARM64_PAN
 	{
 		.desc = "Privileged Access Never",