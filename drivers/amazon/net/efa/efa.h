972c4c9a507aadfd2911f4cfb9fd0492c65692d7
Yonatan Nachum <ynachum@amazon.com>
2026-07-09 11:19:33 +0300

    AL2023 6.12 Update EFA driver to 3.1.0
    Signed-off-by: Yonatan Nachum <ynachum@amazon.com>

--- drivers/amazon/net/efa/efa.h
+++ drivers/amazon/net/efa/efa.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause */
 /*
- * Copyright 2018-2025 Amazon.com, Inc. or its affiliates. All rights reserved.
+ * Copyright 2018-2026 Amazon.com, Inc. or its affiliates. All rights reserved.
  */
 
 #ifndef _EFA_H_
