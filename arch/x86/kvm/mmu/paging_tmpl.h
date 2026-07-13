6f9437745b5dd13a46a31558f52add0b63650eb1
Sean Christopherson <seanjc@google.com>
2026-07-13 08:15:33 -0700

    KVM: x86: Check for invalid/obsolete root *after* making MMU pages available
    Check for a "stale" page fault, i.e. for an invalid and/or obsolete root,
    after making MMU pages available for the shadow MMU.  If reclaiming shadow
    pages zaps an in-use root, i.e. marks it invalid, then KVM will attempt to
    map memory into an invalid root.  On its own, populating an invalid root is
    "fine", but because child shadow pages inherit their parent's role, any
    children created during the map/fetch will be created as invalid pages,
    thus violating KVM's invariant that invalid pages are never on the list of
    active MMU pages.
    
    Note, the underlying flaw has existed since KVM first started tracking
    invalid roots in 2008 (commit 2e53d63acba7, "KVM: MMU: ignore zapped root
    pagetables"), but the true badness only came along in 2020 (Linux 5.9)
    with the invariant that invalid shadow pages can't be on the list of
    active pages.
    
    Note #2, inheriting role.invalid when creating child shadow pages is also
    far from ideal; that flaw will be addressed separately.
    
    Reported-by: Hyunwoo Kim <imv4bel@gmail.com>
    Fixes: f95eec9bed76 ("KVM: x86/mmu: Don't put invalid SPs back on the list of active pages")
    Cc: stable@vger.kernel.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Gyokhan Kochmarla <gyokhan@amazon.com>
    
    [Backport notes for 5.10]
    In 5.10, the code uses is_obsolete_sp() + mmu_notifier_retry() instead of
    is_page_fault_stale(), spin_lock instead of write_lock, and
    make_mmu_pages_available() is called for both TDP-MMU and shadow MMU paths.
    
    Resolution: Moved the is_obsolete_sp() and mmu_notifier_retry() checks after
    make_mmu_pages_available() in both direct_page_fault() and FNAME(page_fault)().
    Added r = RET_PF_RETRY re-initialization after make_mmu_pages_available()
    returns success since it overwrites r. Preserved kvm_mmu_audit() calls.

--- arch/x86/kvm/mmu/paging_tmpl.h
+++ arch/x86/kvm/mmu/paging_tmpl.h
@@ -886,15 +886,18 @@ static int FNAME(page_fault)(struct kvm_vcpu *vcpu, gpa_t addr, u32 error_code,
 
 	r = RET_PF_RETRY;
 	spin_lock(&vcpu->kvm->mmu_lock);
-	if (is_obsolete_sp(vcpu->kvm, to_shadow_page(vcpu->arch.mmu->root_hpa)))
-		goto out_unlock;
-	if (mmu_notifier_retry(vcpu->kvm, mmu_seq))
-		goto out_unlock;
 
 	kvm_mmu_audit(vcpu, AUDIT_PRE_PAGE_FAULT);
 	r = make_mmu_pages_available(vcpu);
 	if (r)
 		goto out_unlock;
+
+	r = RET_PF_RETRY;
+	if (is_obsolete_sp(vcpu->kvm, to_shadow_page(vcpu->arch.mmu->root_hpa)))
+		goto out_unlock;
+	if (mmu_notifier_retry(vcpu->kvm, mmu_seq))
+		goto out_unlock;
+
 	r = FNAME(fetch)(vcpu, addr, &walker, error_code, max_level, pfn,
 			 map_writable, prefault);
 	kvm_mmu_audit(vcpu, AUDIT_POST_PAGE_FAULT);
