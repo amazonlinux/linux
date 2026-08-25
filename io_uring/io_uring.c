ekuiter/torte-linux

io_uring: open code io_put_req_find_next
There is only one user of io_put_req_find_next() and it doesn't make
much sense to have it. Open code the function.

Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
Link: https://lore.kernel.org/r/38b5c5e48e4adc8e6a0cd16fdd5c1531d7ff81a9.1687518903.git.asml.silence@gmail.com
Signed-off-by: Jens Axboe <axboe@kernel.dk>

io_uring/io_uring.c
@@ -1586,22 +1586,6 @@ static void __io_submit_flush_completions(struct io_ring_ctx *ctx)
 	}
 }
 
-/*
- * Drop reference to request, return next in chain (if there is one) if this
- * was the last reference to this request.
- */
-static inline struct io_kiocb *io_put_req_find_next(struct io_kiocb *req)
-{
-	struct io_kiocb *nxt = NULL;
-
-	if (req_ref_put_and_test(req)) {
-		if (unlikely(req->flags & IO_REQ_LINK_FLAGS))
-			nxt = io_req_find_next(req);
-		io_free_req(req);
-	}
-	return nxt;
-}
-
 static unsigned io_cqring_events(struct io_ring_ctx *ctx)
 {
 	/* See comment at the top of this file */
@@ -1954,9 +1938,14 @@ int io_poll_issue(struct io_kiocb *req, struct io_tw_state *ts)
 struct io_wq_work *io_wq_free_work(struct io_wq_work *work)
 {
 	struct io_kiocb *req = container_of(work, struct io_kiocb, work);
+	struct io_kiocb *nxt = NULL;
 
-	req = io_put_req_find_next(req);
-	return req ? &req->work : NULL;
+	if (req_ref_put_and_test(req)) {
+		if (req->flags & IO_REQ_LINK_FLAGS)
+			nxt = io_req_find_next(req);
+		io_free_req(req);
+	}
+	return nxt ? &nxt->work : NULL;
 }
 
 void io_wq_submit_work(struct io_wq_work *work)

amazonlinux/linux

[ Upstream commit 247f97a5f19b642eba5f5c1cf95fc3169326b3fb ]
Stable-dep-off: 20c39819a276 ("io_uring: hold uring_lock when walking link chain in io_wq_free_work()")
[No clean cherry-pick due to missing commit f5c6cf2a310d ("io_uring:
 inline io_free_req()") and commit da1a08c5b281 ("io_uring: introduce
 IO_REQ_LINK_FLAGS"). Instead I manually copied the body of
 io_put_req_find_next as in the original commit. ]
Signed-off-by: Maximilian Heyne <mheyne@amazon.de>

io_uring/io_uring.c
@@ -2403,22 +2403,6 @@ static void io_submit_flush_completions(struct io_ring_ctx *ctx)
 	state->compl_nr = 0;
 }
 
-/*
- * Drop reference to request, return next in chain (if there is one) if this
- * was the last reference to this request.
- */
-static inline struct io_kiocb *io_put_req_find_next(struct io_kiocb *req)
-{
-	struct io_kiocb *nxt = NULL;
-
-	if (req_ref_put_and_test(req)) {
-		if (unlikely(req->flags & (REQ_F_LINK|REQ_F_HARDLINK)))
-			nxt = io_req_find_next(req);
-		__io_free_req(req);
-	}
-	return nxt;
-}
-
 static inline void io_put_req(struct io_kiocb *req)
 {
 	if (req_ref_put_and_test(req))
@@ -6884,9 +6868,14 @@ static int io_issue_sqe(struct io_kiocb *req, unsigned int issue_flags)
 static struct io_wq_work *io_wq_free_work(struct io_wq_work *work)
 {
 	struct io_kiocb *req = container_of(work, struct io_kiocb, work);
+	struct io_kiocb *nxt = NULL;
 
-	req = io_put_req_find_next(req);
-	return req ? &req->work : NULL;
+	if (req_ref_put_and_test(req)) {
+		if (req->flags & (REQ_F_LINK|REQ_F_HARDLINK))
+			nxt = io_req_find_next(req);
+		__io_free_req(req);
+	}
+	return nxt ? &nxt->work : NULL;
 }
 
 static void io_wq_submit_work(struct io_wq_work *work)