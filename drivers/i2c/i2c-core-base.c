c3fbb184a648a49396388069ee839c3523f154da
Johan Hovold <johan@kernel.org>
2026-08-25 08:54:55 +0000

    i2c: core: fix adapter deregistration race
    [ Upstream commit b1a58ed9eab146b36f41a55db8f5d7ce9fdedf3f ]
    
    Adapters can be looked up by their id using i2c_get_adapter() which
    takes a reference to the embedded struct device.
    
    Remove the adapter from the IDR before tearing it down during
    deregistration (and on registration failure) to make sure its resources
    are not accessed after having been freed (e.g. the device name).
    
    Fixes: 35fc37f81881 ("i2c: Limit core locking to the necessary sections")
    Cc: stable@vger.kernel.org	# 2.6.31
    Cc: Jean Delvare <khali@linux-fr.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    [fuzzy picked from v5.15.212 (d39282f552dd)]
    Signed-off-by: AL Kernel Backport Bot <amazon-linux-kernel@amazon.com>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -1475,7 +1475,7 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 	res = device_add(&adap->dev);
 	if (res) {
 		pr_err("adapter '%s': can't register device (%d)\n", adap->name, res);
-		goto err_put_adap;
+		goto err_replace_id;
 	}
 
 	res = of_i2c_setup_smbus_alert(adap);
@@ -1514,7 +1514,10 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 out_reg:
 	i2c_deregister_clients(adap);
 	device_del(&adap->dev);
-err_put_adap:
+err_replace_id:
+	mutex_lock(&core_lock);
+	idr_replace(&i2c_adapter_idr, NULL, adap->nr);
+	mutex_unlock(&core_lock);
 	init_completion(&adap->dev_released);
 	put_device(&adap->dev);
 	wait_for_completion(&adap->dev_released);
@@ -1703,6 +1706,8 @@ void i2c_del_adapter(struct i2c_adapter *adap)
 	/* First make sure that this adapter was ever added */
 	mutex_lock(&core_lock);
 	found = idr_find(&i2c_adapter_idr, adap->nr);
+	if (found == adap)
+		idr_replace(&i2c_adapter_idr, NULL, adap->nr);
 	mutex_unlock(&core_lock);
 	if (found != adap) {
 		pr_debug("attempting to delete unregistered adapter [%s]\n", adap->name);
