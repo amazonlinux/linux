0894356048bcffb12922c90ef8e636ef6e9a66fb
Mark Bundschuh <mkbund@amazon.com>
2026-09-15 21:46:45 +0000

    netfilter: nf_tables: join hook list via splice_list_rcu() in commit phase
    [ Upstream commit a6134e62dba2ea4f760b29d5226907f447c92400 ]
    
    Publish new hooks in the list into the basechain/flowtable using
    splice_list_rcu() to ensure netlink dump list traversal via rcu is safe
    while concurrent ruleset update is going on.
    
    Fixes: 78d9f48f7f44 ("netfilter: nf_tables: add devices to existing flowtable")
    Fixes: b9703ed44ffb ("netfilter: nf_tables: support for adding new devices to an existing netdev chain")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    [ Keep flowtable only. The netdev chain device-update path does not exist in
      this tree, so only the NEWFLOWTABLE commit is converted. ]
    Signed-off-by: Mark Bundschuh <mkbund@amazon.com>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -9994,8 +9994,8 @@ static int nf_tables_commit(struct net *net, struct sk_buff *skb)
 							   nft_trans_flowtable(trans),
 							   &nft_trans_flowtable_hooks(trans),
 							   NFT_MSG_NEWFLOWTABLE);
-				list_splice(&nft_trans_flowtable_hooks(trans),
-					    &nft_trans_flowtable(trans)->hook_list);
+				list_splice_rcu(&nft_trans_flowtable_hooks(trans),
+						&nft_trans_flowtable(trans)->hook_list);
 			} else {
 				nft_clear(net, nft_trans_flowtable(trans));
 				nf_tables_flowtable_notify(&trans->ctx,
